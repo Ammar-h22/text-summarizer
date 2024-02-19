0045ae1897d22b7ab0914428422aa780179d9b1c
Ammar-h22 <husain.ammar22@gmail.com>
2024-02-19 17:56:22 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+from flask import request as req
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/summarize", methods=["GET", "POST"])
+def summarize():
+    input_text = req.form.get("inputText")
+    summarized_text = perform_summarization(input_text)
+    return render_template("index.html", summary=summarized_text["summary_text"])
+
+
+def perform_summarization(text):
+    API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+    headers = {"Authorization": "API_TOKEN"}  # Put your API TOKEN here
+
+    data = req.form["inputText"]
+    minL = 150
+    maxL = int(req.form.get("summaryLength"))
+
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+
+    output = query(
+        {
+            "inputs": data,
+            "parameters": {"min_length": minL, "max_length": maxL},
+        }
+    )[0]
+
+    return output
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
